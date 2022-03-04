301b5479b2d886beae7e3b17579f4cb3fd8f39df
Joane Alves <joanealves2011@gmail.com>
2022-03-04 00:49:04 -0300

    Finished the Html, Css of the Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
- import { GlobalStyle } from "./styles/global";
+ import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
  export function App() {
 
   return (
-    <div className="App">
-      <h1 className='title'>DtMovies</h1>
+    <>
+      <Header />
       <GlobalStyle/>
-    </div>
+    </>
   );
 }
 
