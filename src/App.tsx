d9831332f428398b1a048c713e84daf7206ed8c4
Joane Alves <joanealves2011@gmail.com>
2022-03-05 21:29:05 -0300

    Modal styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
 
@@ -20,15 +21,12 @@ export function App() {
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
-      <Dashboard/>
-
-      <Modal 
-               isOpen={isNewTransactionModalOpen}
-               onRequestClose={handleCloseNewTransactionModal}
-               >
-                <h2>Cadastrar transação</h2>
-              </Modal>
-      <GlobalStyle/>
+      <Dashboard />
+      <NewTransactionModal 
+        isOpen ={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
     </>
   );
 }
