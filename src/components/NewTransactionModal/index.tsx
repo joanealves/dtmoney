be0fddfc164c66c44ab1b3b602cc2ec2f890de0d
Joane Alves <joanealves2011@gmail.com>
2022-03-06 17:21:03 -0300

    List of Transactions and Seeds

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,8 @@ import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from'../../assets/close.svg'
 
+import { api }  from '../../services/api';
+
 import { Container, TransactionTypeContainer,RadioBOx } from './style';
 
 interface NewTransactionModalProps {
@@ -16,9 +18,19 @@ export function NewTransactionModal({isOpen, onRequestClose }: NewTransactionMod
     const [value, setValue] = useState(0);
     const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit');
+    
  
     function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
+
+        const data = {
+            title, 
+            value, 
+            category, 
+            type, 
+        };
+
+        api.post('transactions', data)
     }
 
     return (        
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
+
 export function TransactionsTable() {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
     useEffect(() => {
         api.get('transactions')
-        .then(data => console.log (data))
+        .then(response => setTransactions(response.data.transactions))
     }, [])
 
     return(
@@ -20,18 +31,15 @@ export function TransactionsTable() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de web site</td>
-                        <td className="deposit">R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20.02.22</td>
+                   {transactions.map(transaction => (
+                        <tr key={transaction.id}>
+                        <td>{transaction.title}</td>
+                        <td className={transaction.type}>{transaction.amount}</td>
+                        <td>{transaction.category}</td>
+                        <td>{transaction.createdAt}</td>
                     </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withraw"> -R$1.100</td>
-                        <td>Casa</td>
-                        <td>17.02.22</td>
-                    </tr>
+                   ))}
+               
                 </tbody>
             </table>
         </Container>
