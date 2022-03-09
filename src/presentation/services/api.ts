6908034156fc73a947166f129a9ed851065d8ac5
Joane Alves <joanealves2011@gmail.com>
2022-03-09 13:54:00 -0300

    Adjustment baseUrl

--- src/presentation/services/api.ts
+++ src/presentation/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://github.com/joanealves/dtmoney/api',
+    baseURL: 'https://joanealves.github.io/dtmoney/api',
 })
